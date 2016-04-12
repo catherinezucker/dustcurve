4f8d9f93a170b49ca183e93d6b567c3078139d92
catherinezucker <catherine.zucker@cfa.harvard.edu>
2016-04-11 23:14:05 -0400

    Create test.py

---
+++ dustcurve/tests/test.py
@@ -0,0 +1,9 @@
+
+from unittest import TestCase
+
+import dustcurve
+
+class TestFillerFunc(TestCase):
+    def test_is_string(self):
+        s = dustcurve.fillerfunc()
+        self.assertTrue(isinstance(s, basestring))
