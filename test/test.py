d6de5f8efabd0df5ce57f73d6160934318774b1b
catherinezucker <catherine.zucker@cfa.harvard.edu>
2016-04-11 23:14:25 -0400

    Delete test.py

--- test/test.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-import dustcurve
-
-class TestFillerFunc(TestCase):
-    def test_is_string(self):
-        s = dustcurve.fillerfunc()
-        self.assertTrue(isinstance(s, basestring))
