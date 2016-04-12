0b70de1a275d9ba0ee46747418ea351b87f574b2
catherinezucker <catherine.zucker@cfa.harvard.edu>
2016-04-11 23:10:11 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import dustcurve
 
-class TestJoke(TestCase):
+class TestFillerFunc(TestCase):
     def test_is_string(self):
         s = dustcurve.fillerfunc()
         self.assertTrue(isinstance(s, basestring))
