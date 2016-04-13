f4d26561a6dbb1e6a621db23c2d10e09f7bf8eba
catherinezucker <catherine.zucker@cfa.harvard.edu>
2016-04-13 19:58:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@ from setuptools import setup
 
 setup(name='dustcurve',
       version='0.1',
-      description='Create Dust-Refined Galactic Rotation Curve',
-      url='http://github.com/p201-sp2016/Refining_Kinematic_Distance_Estimates',
+      description='Refine Galactic Rotation Curve using Dust',
+      url='http://github.com/phys201-sp2016/dustcurve',
       author='Catherine Zucker',
-      author_email='catherine.zucker@cfa.harvard.edu,
-      license='None',
+      license='GNU Public License',
+      author_email='catherine.zucker@cfa.harvard.edu',
       packages=['dustcurve'],
       install_requires=[
           'numpy',
+          'matplotlib',
+          'emcee',
+          'scipy'
       ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
       zip_safe=False)
