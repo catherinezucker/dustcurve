6823626c9a75e304a136c800b96bd0cd40ca273e
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-04-17 22:04:16 -0400

    Update io.py

--- dustcurve/io.py
+++ dustcurve/io.py
@@ -1,5 +1,8 @@
 import h5py
 
+#adapted from class in https://github.com/bayestar/hdf5io.py
+
+#this class reads in data from a repackaged hdf5 file and stores the stellar posteriors and co data from stars in the given pixel
 class PixStars:
 	def __init__(self, fname=None, pix=None):
 		f = None
@@ -29,17 +32,21 @@ class PixStars:
 		self.nImages = dpdfs.shape[0]
 		self.p = dpdfs[:,:,:]
 		self.co=dco[:,:]
-		
+	
+	#return the stellar posteriors for all the stars in the pixel
 	def get_p(self, imgIdx=None):
 		if imgIdx == None:
 			return self.p
 		else:
 			return self.p[imgIdx]
+			
+	#return the co_data for all the stars in the pixel
 	def get_co(self, imgIdx=None):
 		if imgIdx == None:
 			return self.co
 		else:
 			return self.co[imgIdx]
 	
+	#return the number of stars in the pixel
 	def get_n_stars(self):
 		return self.nImages
