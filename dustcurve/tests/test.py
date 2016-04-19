a80dca12fde124ba46aa0bd0e4dba224af23fd2b
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-04-18 22:24:47 -0400

    Update test.py

--- dustcurve/tests/test.py
+++ dustcurve/tests/test.py
@@ -9,5 +9,9 @@ class PixStarsTestCase(unittest.TestCase):
    def TestPixStars(self):
       """Tests PixClass class"""
       #Create PixClass object
-      pixObj = PixClass('testdata.h5', 'pixel 1024-5753839')
+      pixTest = PixClass('testdata.h5', 'pixel 1024-5753839')
       # Is the stellar posterior array the right shape? 
+      self.assertTrue(pixTest.get_p()[0,:,:].shape==(120,700))
+if __name__ == '__main__':
+    unittest.main()
+
