a95f2764ec73d3a763a81d4bba937ba4c118d102
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-04-19 20:31:08 -0400

    Edited nwalkers and nsteps

--- runmcmc.py
+++ runmcmc.py
@@ -8,16 +8,15 @@ import numpy as np
 # the model has 12 parameters; we'll use 50 walkers and 500 steps each
 ndim = 12
 nwalkers = 50
-nsteps = 1000
+nsteps = 50
 
 # set up the walkers in a "Gaussian ball" around the literature estimate for distance to Cepheus cloud (distance mod=10)
-ls_result = np.ones((12))*10.0  
+ls_result = np.arange(4,16,1)
 starting_positions = [ls_result + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
 
 #pixObj=pixclass.PixStars('testdata.h5', 'pixel 1024-5753839')
-
-#co_array=np.asarray(pixObj.get_co()[:,:])
-#post_array=np.asarray(pixObj.get_p()[:,:,:])
+#co_array=np.asarray(pixObj.get_co()[0,:])
+#post_array=np.asarray(pixObj.get_p()[0,:,:])
 #nstars=np.asarray(pixObj.get_n_stars())
 
 
