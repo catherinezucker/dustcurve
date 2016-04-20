534c3c6a272a0cc9eca0265c2086a5e34ce2b689
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-04-19 23:32:22 -0400

    Tried to plot markov chains for some of the parameters

--- runmcmc.py
+++ runmcmc.py
@@ -4,14 +4,17 @@ import healpy
 import emcee 
 from dustcurve import model 
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # the model has 12 parameters; we'll use 50 walkers and 500 steps each
 ndim = 12
-nwalkers = 50
-nsteps = 1000
+nwalkers = 24
+nsteps = 100
 
 # set up the walkers in a "Gaussian ball" around the literature estimate for distance to Cepheus cloud (distance mod=10)
-ls_result = np.arange(4,16,1)
+#ls_result = np.arange(4,16,1)
+ls_result=np.arange(4,16,1)
 starting_positions = [ls_result + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
 
 # set up the sampler object
@@ -22,3 +25,12 @@ sampler = emcee.EnsembleSampler(nwalkers, ndim, model.log_posterior, args=['pixe
 sampler.run_mcmc(starting_positions, nsteps)
 
 print('Done')
+
+fig, (ax_d1, ax_d2) = plt.subplots(2)
+ax_d1.set(ylabel='d1')
+ax_d2.set(ylabel='d2')
+for i in range(10):
+    sns.tsplot(sampler.chain[i,:,0], ax=ax_d1)
+    sns.tsplot(sampler.chain[i,:,1], ax=ax_d2)
+    
+plt.show()
