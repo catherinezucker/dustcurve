a9f70350c86a9a9e356e8e957e179d2404d3f5c7
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-04-26 14:37:01 -0400

    Update test.py

--- dustcurve/tests/test.py
+++ dustcurve/tests/test.py
@@ -22,7 +22,7 @@ class ModelLineIntegralTestCase(unittest.TestCase):
       ndim=12
       nsteps=1
       nwalkers=1
-      pos_array=[4,4,4,4,4,4,7.75,4,4,4,14,4]
+      pos_array=[5,5,5,5,5,5,7.75,5,5,5,14,5]
       std_array=[1. for i in range(ndim)]
       starting_positions = emcee.utils.sample_ball((pos_array),(std_array),nwalkers) #set up the initial 
       file='simulated_data.h5'
