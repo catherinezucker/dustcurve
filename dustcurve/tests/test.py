338806de3edae716d12f35ae8dadfc28e59b64d4
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-04-26 14:03:19 -0400

    Update test.py

--- dustcurve/tests/test.py
+++ dustcurve/tests/test.py
@@ -11,34 +11,17 @@ class PixStarsTestCase(unittest.TestCase):
    def TestPixStars(self):
       """Tests PixClass class"""
       #Create PixClass object
-      pixTest = pixclass.PixStars('testdata.h5', 'pixel 1024-5753839')
+      pixTest = pixclass.PixStars('simulated_data.h5')
       # Is the stellar posterior array the right shape? 
-      self.assertTrue(pixTest.get_p()[0,:,:].shape==(120,700))
+      self.assertTrue(pixTest.get_p()[0,:,:].shape==(700,120))
       
 class ModelLineIntegralTestCase(unittest.TestCase): 
    def TestLineIntegral(self):
       """Tests Line Integral Function Critical to Model"""
-      #create artificial arrays we fill with known values
-      post_array=np.zeros((120,700))
-      dist_array=np.array([4.3,5.5,6.2,7,8.3,9.1,10,10,11.45,12.9,13.7,14.01])
-      coeff_array=np.ones((12))
-      co_array=np.ones((12))*0.25
-      dbins,redbins=model.convert_to_bins(co_array,dist_array,coeff_array)
-       #manipulate the posterior array so we know exactly what the line integral should be, given the dist_array, co_array, coeff_array   
-      post_array[0:2,0]=1
-      post_array[2:12,25]=1
-      post_array[12:17,50]=1
-      post_array[17:24,75]=1
-      post_array[24:34,100]=1
-      post_array[34:40,125]=1
-      post_array[40:48,150]=1
-      post_array[48:59,200]=1
-      post_array[59:71,225]=1
-      post_array[71:77,250]=1
-      post_array[77:80,275]=1
-      post_array[80:-1,300]=1
-      #test that the value of the integral over the stellar posterior array is 1 for this specific test case
-      self.assertTrue(model.get_line_integral(co_array,post_array,dist_array,coeff_array)==119.0)
+      pixTest = pixclass.PixStars('simulated_data.h5')
+      #check that the line integral you're getting is above 215, the approximate probability you would get if you summed
+      #over the "true" reddening profile 
+      self.assertTrue(model.get_line_integral(co_array,post_array,dist_array,coeff_array)>215.00)
       
 if __name__ == '__main__':
     unittest.main()
