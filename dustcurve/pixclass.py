8796a584a2dde894388110d80b17f083c2643f06
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-04-25 21:02:24 -0400

    Update pixclass.py

--- dustcurve/pixclass.py
+++ dustcurve/pixclass.py
@@ -3,7 +3,7 @@ import h5py
 #adapted from class in https://github.com/bayestar/hdf5io.py
 #this class reads in data from a repackaged hdf5 file and stores the stellar posteriors and co data from stars in the given pixel
 class PixStars:
-	def __init__(self, fname=None, pix=None):
+	def __init__(self, fname=None):
 		f = None
 		close = False
 		
@@ -16,8 +16,8 @@ class PixStars:
 			else:
 				f = h5py.File(fname, 'r')
 				close = True
-			dpdfs = f[pix+'/stellar_pdfs']
-			dco = f[pix+'/co_data']
+			dpdfs = f['/stellar_pdfs']
+			dco = f['/co_data']
 		
 		if pix == None:
 			raise ValueError('A dataset name or object must be provided.')
