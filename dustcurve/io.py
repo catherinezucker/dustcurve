7ba1d157fe36ba1e5433d8286b34436f070f9286
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-05-04 18:26:49 -0400

    Update io.py

--- dustcurve/io.py
+++ dustcurve/io.py
@@ -7,9 +7,9 @@ def fetch_args(fname, bounds, ratio):
              pdata= the prior arguments for running emcee's PTSampler ("logpargs")
              
     Parameters:
-        fname: filename of the pixel you want to read in
-        the bounds you'd like for your uniform prior
-        ratio: the gas-to-dust conversion coefficient you'd like to adopt
+        fname: filename of the pixel you want to read in (a string)
+        the bounds you'd like for your uniform prior (given in the format [lowerbound, upperbound]); must be confined to within 4-19 in distance modulus
+        ratio: the gas-to-dust conversion coefficient you'd like to adopt (a float)
     """
     pixObj=pixclass.PixStars(fname)
     co_array=np.asarray(pixObj.get_co()[:,:])
