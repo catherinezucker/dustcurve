cc11e395e8ed591b1663cf69f8d49a465416fc61
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-05-05 19:42:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(name='dustcurve',
       author='Catherine Zucker',
       author_email='catherine.zucker@cfa.harvard.edu',
       packages=['dustcurve'],
-      requires=['h5py', 'matplotlib', 'emcee', 'numpy', 'scipy', 'seaborn', 'pandas'],
       install_requires=[
           'h5py>=2.5.0',
           'matplotlib',
@@ -17,7 +16,6 @@ setup(name='dustcurve',
           'seaborn',
           'pandas'
       ],
-      dependency_links=['https://github.com/h5py/h5py.git'],
       test_suite='nose.collector',
       tests_require=['nose'],
       zip_safe=False)
