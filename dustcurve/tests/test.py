f55bfde6dcab5de42ac658dfc75d8b345c739857
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-05-04 20:43:13 -0400

    Update test.py

--- dustcurve/tests/test.py
+++ dustcurve/tests/test.py
@@ -1,13 +1,12 @@
-
 from unittest import TestCase
 import unittest
 from dustcurve import pixclass
 from dustcurve import model
 import numpy as np
 import emcee
+from dustcurve import io
 
-# Test PixStars Class
-
+#test pixstars class
 class PixStarsTestCase(unittest.TestCase): 
    def TestPixStars(self):
       """Tests PixClass class"""
@@ -15,35 +14,46 @@ class PixStarsTestCase(unittest.TestCase):
       pixTest = pixclass.PixStars('simulated_data.h5')
       # Is the stellar posterior array the right shape? 
       self.assertTrue(pixTest.get_p()[0,:,:].shape==(700,120))
+      #Is the CO data array the right shape?
+      self.assertTrue(pixTest.get_co()[0,:].shape==(12))
       
+      
+#test io module
+class IOTestCase(unittest.TestCase): 
+   def TestIO(self):
+      """Tests PixClass class"""
+      #Call io.fetch_args
+      ldata,pdata = io.fetch_args('simulated_data.h5', [4,19], 1.0)
+      # Are the arguments returned in the right format? 
+      self.assertTrue(pdata.shape==(2,1))
+      self.assertTrue(
+      #Is the CO data array the right shape?
+      self.assertTrue(pixTest.get_co()[0,:].shape==(12))
+      
+
+#test model module      
 class ModelLineIntegralTestCase(unittest.TestCase): 
    def TestLineIntegral(self):
-      """Tests Line Integral Function Critical to Model"""
-      ncoeff = 12
-      ndist= 12
-      ndim=24
-      nwalkers = 50
-      nsteps = 500
-      
-      fname='simulated_data.h5'
-      pixObj=pixclass.PixStars(fname)
-      co_array=np.asarray(pixObj.get_co()[:,:])
-      post_array=np.asarray(pixObj.get_p()[:,:,:])
-      nstars=pixObj.get_n_stars()
-      data=(co_array,post_array,nstars) 
+        ndim=12
+        nwalkers = 50
+        nsteps = 1
 
-      allsamples = np.empty((1,ndim))
-      pos_array=[4,4.5,4.75,5,5,5,7.75,8,8,8,14,15] #set off walkers at random distances within acceptable range
-      pos_array.extend([1 for i in range(ncoeff)]) #set of walkers near expected gas to dust coefficient in literatre (0.03 mags of reddening per CO intensity unit K)
-      std_array=[0.01 for i in range(ndim)]
-      starting_positions = emcee.utils.sample_ball((pos_array),(std_array),nwalkers) #set up the initial position vectors for our walkers
+        #fetch the required likelihood and prior arguments for PTSampler
+        ldata,pdata=io.fetch_args('simulated_data.h5',[4,19],1.0)
 
-      sampler = emcee.MHSampler(np.diagflat(np.ones(ndim)), ndim, model.log_posterior, args=(data))
-      
-      (pos,lnprob,rstate)=sampler.run_mcmc(starting_positions[0],nsteps)
-      #check that the line integral you're getting is above 215, the approximate probability you would get if you summed
-      #over the "true" reddening profile given by the above distance array
-      self.assertTrue(lnprob>215.00)
+        #set up the walkers at a location where we know what probability should be returned
+        result=[5,5,5,5,5,5,7.75,8,8,8,14,15]
+
+        #set up starting positions, all at same distance 
+        starting_positions = [[result for i in range(nwalkers)] for j in range(ntemps)]
+
+        #set up the sampler object
+        sampler = emcee.PTSampler(ntemps, nwalkers, ndim, model.log_likelihood, model.log_prior, loglargs=(ldata), logpargs=[pdata])
+     
+        (pos,lnprob,rstate)=sampler.run_mcmc(starting_positions,nsteps)
+        #check that the line integral you're getting is above 215, the approximate probability you would get if you summed
+        #over the "true" reddening profile given by the above distance array
+        self.assertTrue(lnprob>215.00)
       
 if __name__ == '__main__':
     unittest.main()
