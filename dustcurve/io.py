3c630b5fdb2996cc1f11647f7d3d3f212c639abc
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-06-01 14:20:35 -0400

    Update io.py

--- dustcurve/io.py
+++ dustcurve/io.py
@@ -30,10 +30,10 @@ def fetch_args(fnames, bounds, ratio):
             indices.append(unique_stars)
 
 
-	#make unique posterior arrays
-	unique_post = []
-	for i in range(unique_co.shape[0]):
-		unique_post.append(post_array[indices[i],:,:][0])
+    #make unique posterior arrays
+    unique_post = []
+    for i in range(unique_co.shape[0]):
+        unique_post.append(post_array[indices[i],:,:][0])
 
         #package and return data
         ldata=(unique_co,indices,unique_post,ratio)
@@ -66,9 +66,9 @@ def fetch_args(fnames, bounds, ratio):
             unique_stars=np.asarray(np.where(np.equal(unique_co[i], co_all).all(axis=1)))
             indices.append(unique_stars)
 
-	unique_post = []
-	for i in range(unique_co.shape[0]):
-		unique_post.append(post_all[indices[i],:,:][0])
+    unique_post = []
+    for i in range(unique_co.shape[0]):
+        unique_post.append(post_all[indices[i],:,:][0])
 
         #package and return data
         ldata=(unique_co,indices,unique_post,ratio)
