a9f2d60f213530682cd747db139fe55e44c5e8c7
Catherine Zucker <catherine.zucker@cfa.harvard.edu>
2016-06-01 21:53:29 -0400

    plotting all possible reddening profiles except just 1; changed opacity of lines

--- dustcurve/plot_posterior.py
+++ dustcurve/plot_posterior.py
@@ -4,7 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_posterior(post_array,xpts,ypts,best,ratio,unique_co):
+def plot_posterior(post_array,xpts,ypts,best,ratio,unique_co,x_range,y_range):
     #normalize stellar posterior surfaces
     dispsurf = post_array.copy()
     dispsurf[~np.isfinite(dispsurf)] = 0
@@ -21,24 +21,28 @@ def plot_posterior(post_array,xpts,ypts,best,ratio,unique_co):
         dx = np.median(xpts[1:]-xpts[:-1])
         dy = np.median(ypts[1:]-ypts[:-1])
         extent = [xpts[0]-dx/2., xpts[-1]+dx/2.,ypts[0]-dy/2., ypts[-1]+dy/2.]
-    ax.imshow(totsurf,origin='lower',aspect='auto', cmap='binary', interpolation='nearest',extent=extent, vmin=0,vmax=0.03)
+    ax.imshow(totsurf,origin='lower',aspect='auto', cmap='binary', interpolation='nearest',extent=extent, vmin=0,vmax=1)
     ax.autoscale(False)
+
 
     #determine the reddening profile
     distances=best[0:int(len(best)/2)]
-    reddenings=np.cumsum(np.multiply(best[int(len(best)/2):],ratio))
-    
+    coefficients=best[int(len(best)/2):]
     order=np.argsort(distances)
-    unique_co=unique_co[0][order]
     distances=distances[order]
     distances=np.around(distances,2)
     reddenings=np.cumsum(np.multiply(unique_co,ratio))
     
+    for i in range(0,len(unique_co)):
     #convert the actual distance and reddening into something plottable
-    repeats=np.around((np.ediff1d(distances,to_end=19.0-distances[-1],to_begin=distances[0]-4)/0.01),0)
-    profiley=np.repeat(np.insert(reddenings,0,0),repeats.astype(int))
-    profilex=np.linspace(4,19,1500)
+        unique_co[i]=unique_co[i][order]
+        reddenings=np.cumsum(np.multiply(unique_co[i],ratio))
+        repeats=np.around((np.ediff1d(distances,to_end=19.0-distances[-1],to_begin=distances[0]-4)/0.01),0)
+        profiley=np.repeat(np.insert(reddenings,0,0),repeats.astype(int))
+        profilex=np.linspace(4,19,1500)
 
     #plot
-    ax.plot(profilex,profiley)
+        ax.plot(profilex,profiley,alpha=0.15)
+        ax.set_xlim(x_range)
+        ax.set_ylim(y_range)
     
